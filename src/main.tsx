6d55e09748fd16fda940935f02efa3dbe4b74b86
imornar <imornar@gmail.com>
2023-08-04 18:36:30 +0200

    feat: add venly

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { WagmiConfig, configureChains, createConfig, mainnet } from "wagmi";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
+import { VenlyConnector } from "@venly/wagmi-connector";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { bscTestnet } from "wagmi/chains";
 
-const { publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [bscTestnet],
   [publicProvider()]
 );
 
@@ -14,6 +17,16 @@ const config = createConfig({
   autoConnect: true,
   publicClient,
   webSocketPublicClient,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new VenlyConnector({
+      chains,
+      options: {
+        clientId: "2c81c239-6c49-4d70-a087-d10dca892d9b",
+        environment: "sandbox",
+      },
+    }),
+  ],
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
